d58afa06f9c34a1492e3474e2b086bc729d8bd97
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-05-12 15:25:33 +0200

    nouveau fichier pour gerer les mails qui devront etre envoyé

---
+++ src/ApiResource/ContactRequest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ApiResource;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Post;
+use App\state\GroqProcessor;
+
+#[ApiResource()]
+class ContactRequest
+{
+}
