22bedcffc3f7bdbce5b814052cd740d56e5f4c67
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-05-29 11:24:31 +0200

    processor pour la route verification code

---
+++ src/state/UserVerificationProcessor.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace App\state;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProcessorInterface;
+
+class UserVerificationProcessor implements ProcessorInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = [])
+    {
+    }
+}
