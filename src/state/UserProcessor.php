b1baf3f441fe7978313ac06f95a43fcd334a3c43
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-05-29 11:09:06 +0200

    ajout de la verification du mail académique pour envoyer le code de verification

--- src/state/UserProcessor.php
+++ src/state/UserProcessor.php
@@ -8,24 +8,41 @@ use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 use App\Entity\User;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class UserProcessor implements ProcessorInterface
 {
     public function __construct(
         #[Autowire(service: 'api_platform.doctrine.orm.state.persist_processor')]
         private ProcessorInterface $persistProcessor,
-    ) {
+        private MailerInterface $mailer)
+    {
     }
 
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = [])
     {
         if ($data instanceof User) {
             if (in_array('ROLE_USER_PROF', $data->getRoles())) {
+                $emailProf = $data->getMailAcademique();
+                if (!preg_match('/@(ac-[a-z]+|education)\.gouv?\.fr$/i', $emailProf)
+                    || preg_match('/(eleve|etudiant|student|lycee|clg)/i', $emailProf)) {
+                    throw new \Symfony\Component\HttpKernel\Exception\BadRequestHttpException("L'adresse email fournie n'est pas une adresse académique réservée aux enseignants.");
+                }
+
                 $codeVerification = (string) random_int(100000, 999999);
                 $data->setCodeVerif($codeVerification);
                 $expiration = (new \DateTime())->modify('+15 minutes');
                 $data->setDateExpiration($expiration);
                 $data->setStatutVerification(false);
+
+                $email = (new Email())
+                    ->from('noreply@litterIA.com')
+                    ->to($emailProf)
+                    ->subject('Votre code de vérification Enseignant')
+                    ->html('<p>Bonjour,</p><p>Voici votre code de vérification pour finaliser votre inscription : <strong>'.$codeVerification.'</strong></p><p>Ce code est valable pendant 15 minutes.</p>');
+
+                $this->mailer->send($email);
             }
         }
 
