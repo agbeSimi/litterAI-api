394c8c10a2899d31b3ec670ea874c1b3cc219111
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-05-29 11:49:18 +0200

    code pour verifier le code et le temps d'expiration pour donner access au role professeur

--- src/state/UserVerificationProcessor.php
+++ src/state/UserVerificationProcessor.php
@@ -1,18 +1,44 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\state;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class UserVerificationProcessor implements ProcessorInterface
 {
+    public function __construct(
+        #[Autowire(service: 'api_platform.doctrine.orm.state.persist_processor')]
+        private ProcessorInterface $persistProcessor,
+        private UserRepository $userRepository,
+    ) {
+    }
 
-    /**
-     * @inheritDoc
-     */
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = [])
     {
+        if ($data instanceof User) {
+            // 1. On récupère le vrai utilisateur complet en BDD grâce au login envoyé par React
+            $userComplet = $this->userRepository->findOneBy(['login' => $data->getLogin()]);
+
+            // Si l'utilisateur n'existe pas en base de données
+            if (!$userComplet) {
+                throw new NotFoundHttpException('Utilisateur non trouvé.');
+            }
+
+            if ($data->getCodeVerif() === $userComplet->getCodeVerif()
+                && new \DateTime() < $userComplet->getDateExpiration()) {
+                $userComplet->setStatutVerification(true);
+                $userComplet->setCodeVerif(null);
+            }
+            return $this->persistProcessor->process($userComplet, $operation, $uriVariables, $context);
+        }
+
+        return $this->persistProcessor->process($data, $operation, $uriVariables, $context);
     }
 }
