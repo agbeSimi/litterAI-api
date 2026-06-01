22db576107de5393dab33ef0ee5e77fdd5e26d52
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-06-01 14:35:43 +0200

    nouvelle entité classe lié a la classe USER a travers prof et eleve

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,6 +7,8 @@ use ApiPlatform\Metadata\Post;
 use App\Repository\UserRepository;
 use App\state\UserProcessor;
 use App\state\UserVerificationProcessor;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -67,6 +69,20 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $email = null;
+
+    /**
+     * @var Collection<int, Classe>
+     */
+    #[ORM\OneToMany(targetEntity: Classe::class, mappedBy: 'professeur')]
+    private Collection $classes;
+
+    #[ORM\ManyToOne(inversedBy: 'eleves')]
+    private ?Classe $classe = null;
+
+    public function __construct()
+    {
+        $this->classes = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -205,6 +221,48 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setEmail(string $email): static
     {
         $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Classe>
+     */
+    public function getClasses(): Collection
+    {
+        return $this->classes;
+    }
+
+    public function addClass(Classe $class): static
+    {
+        if (!$this->classes->contains($class)) {
+            $this->classes->add($class);
+            $class->setProfesseur($this);
+        }
+
+        return $this;
+    }
+
+    public function removeClass(Classe $class): static
+    {
+        if ($this->classes->removeElement($class)) {
+            // set the owning side to null (unless already changed)
+            if ($class->getProfesseur() === $this) {
+                $class->setProfesseur(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getClasse(): ?Classe
+    {
+        return $this->classe;
+    }
+
+    public function setClasse(?Classe $classe): static
+    {
+        $this->classe = $classe;
 
         return $this;
     }
