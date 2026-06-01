d040aad704ee6ce5fb4087d22bcc63fdae9b2c33
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-06-01 14:31:23 +0200

    nouvelle entité classe

---
+++ src/Entity/Classe.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClasseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClasseRepository::class)]
+class Classe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $nomPurifie = null;
+
+    #[ORM\Column]
+    private ?int $effectif = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getNomPurifie(): ?string
+    {
+        return $this->nomPurifie;
+    }
+
+    public function setNomPurifie(string $nomPurifie): static
+    {
+        $this->nomPurifie = $nomPurifie;
+
+        return $this;
+    }
+
+    public function getEffectif(): ?int
+    {
+        return $this->effectif;
+    }
+
+    public function setEffectif(int $effectif): static
+    {
+        $this->effectif = $effectif;
+
+        return $this;
+    }
+}
