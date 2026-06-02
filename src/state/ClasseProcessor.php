a0d1623736b10f5e939dd2b350a92de6554c4388
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-06-02 12:08:02 +0200

    ajout d'autoWire

--- src/state/ClasseProcessor.php
+++ src/state/ClasseProcessor.php
@@ -9,12 +9,14 @@ use ApiPlatform\State\ProcessorInterface;
 use App\Entity\Classe;
 use App\Entity\User;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class ClasseProcessor implements ProcessorInterface
 {
     public function __construct(
+        #[Autowire(service: 'api_platform.doctrine.orm.state.persist_processor')]
         private ProcessorInterface $persistProcessor,
         private Security $security,
         private UserPasswordHasherInterface $passwordHasher,
