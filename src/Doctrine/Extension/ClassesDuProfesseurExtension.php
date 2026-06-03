27bc768242f56ef1f81237fe3fc1cf0dfb9c15a4
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-06-03 12:27:37 +0200

    cette classe sers d'intercepteur de requete sql, si c'est un professeur il envoi seulement les classes de ce professeur en question

--- src/Doctrine/Extension/ClassesDuProfesseurExtension.php
+++ src/Doctrine/Extension/ClassesDuProfesseurExtension.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Doctrine\Extension;
@@ -6,16 +7,34 @@ namespace App\Doctrine\Extension;
 use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
 use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use ApiPlatform\Metadata\Operation;
+use App\Entity\Classe;
 use Doctrine\ORM\QueryBuilder;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class ClassesDuProfesseurExtension implements QueryCollectionExtensionInterface
 {
+    private Security $security;
 
-    /**
-     * @inheritDoc
-     */
+    // On injecte le service Security de Symfony pour connaître l'utilisateur connecté
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, ?Operation $operation = null, array $context = []): void
     {
-        // TODO: Implement applyToCollection() method.
+        if (Classe::class !== $resourceClass) {
+            return;
+        }
+        if ($this->security->isGranted('ROLE_ADMIN')) {
+            return;
+        }
+
+        $user = $this->security->getUser();
+        if ($user && in_array('USER_ROLE_PROFESSEUR', $user->getRoles())) {
+            $rootAlias = $queryBuilder->getRootAliases()[0];
+            $queryBuilder->andWhere($rootAlias.'.professeur = :prof');
+            $queryBuilder->setParameter('prof', $user);
+        }
     }
 }
