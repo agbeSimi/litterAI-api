96c59211097423987caf7c454bca419b0b4a02ef
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-06-04 14:04:13 +0200

    Revert "fix: utilisation de schema:update pour postgresql"
    This reverts commit 712893193c52d2b76f9dbb401862f0ddf160f324.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,5 @@ set -e
 php bin/console cache:clear --no-warmup
 php bin/console cache:warmup
 php bin/console assets:install public
-php bin/console doctrine:schema:update --force
+php bin/console doctrine:migrations:migrate --no-interaction
 exec apache2-foreground
