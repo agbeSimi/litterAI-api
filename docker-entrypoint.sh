712893193c52d2b76f9dbb401862f0ddf160f324
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-06-04 09:50:46 +0000

    fix: utilisation de schema:update pour postgresql

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,5 @@ set -e
 php bin/console cache:clear --no-warmup
 php bin/console cache:warmup
 php bin/console assets:install public
-php bin/console doctrine:migrations:migrate --no-interaction
+php bin/console doctrine:schema:update --force
 exec apache2-foreground
