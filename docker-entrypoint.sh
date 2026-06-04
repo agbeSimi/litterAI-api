009654aba512bb6364ea30f0c0256e9ec17701de
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-06-04 09:43:09 +0000

    fix: suppression de l option env=prod obsolète

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-php bin/console cache:clear --env=prod --no-warmup
-php bin/console cache:warmup --env=prod
-php bin/console assets:install public --env=prod
-php bin/console doctrine:migrations:migrate --no-interaction --env=prod
+php bin/console cache:clear --no-warmup
+php bin/console cache:warmup
+php bin/console assets:install public
+php bin/console doctrine:migrations:migrate --no-interaction
 exec apache2-foreground
